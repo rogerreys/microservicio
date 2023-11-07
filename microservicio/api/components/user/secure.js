c293a1b2fa1af0682919c572f36b5052494767af
Roger Reyes <rreyes@cobiscorp.com>
2023-11-07 06:53:45 -0500

    Token

---
+++ microservicio/api/components/user/secure.js
@@ -0,0 +1,15 @@
+const auth = require('../../../auth');
+
+module.exports = function checkAuth(action) {
+    function middleware(req, res, next) {
+        switch (action) {
+            case 'upsert':
+                const owner = req.body.id;
+                auth.check.own(req, owner);
+                break;
+            default:
+                next()
+        }
+    }
+    return middleware
+}
--- microservicio/auth/index.js
+++ microservicio/auth/index.js
@@ -1,8 +1,46 @@
-const jwt = require("jsonwebtoken")
-function sign(data){
-    return jwt.sign(data, "secret")
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+
+const secret = config.jwt.secret;
+
+function sign(data) {
+    return jwt.sign(data, secret)
+}
+
+const check = {
+    own: function (req, owner) {
+        const decoded = decodeHeader(req);
+        console.log("decoded: ", decoded);
+    }
+}
+
+function getToken(auth) {
+    // Bearer
+    if (!auth) {
+        throw new Error('No viene token');
+    }
+    if (auth.indexOf('Bearer') === -1) {
+        throw new Error('Formato invalido');
+    }
+
+    let token = auth.replace("Bearer", '');
+
+    return token;
+}
+
+function verify(token) {
+    return jwt.verify(token, secret);
+}
+
+function decodeHeader(req) {
+    const authorization = req.headers.authorization || '';
+    const token = getToken(authorization);
+    const decode = verify(token);
+    req.user = decode
+    return decode;
 }
 
 module.exports = {
-    sign
+    sign,
+    check
 }
